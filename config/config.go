c08b897e1f04d32c12062fa672166cbc0d0a1def
Jerry <jerryyoung2911@gmail.com>
2022-07-06 09:11:45 +0700

    Add env config

--- config/config.go
+++ config/config.go
@@ -5,16 +5,45 @@ import (
 	"go-db/entity"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Setting struct {
+	DBUser     string
+	DBPassword string
+	DBHost     string
+	DBPort     int16
+	DBName     string
+}
+
+func ReadFromEnv() Setting {
+	res := Setting{}
+	err := godotenv.Load("local.env")
+	if err != nil {
+		return Setting{}
+	}
+	res.DBUser = os.Getenv("DBUser")
+	res.DBPassword = os.Getenv("DBPass")
+	res.DBHost = os.Getenv("DBHost")
+	portConv, _ := strconv.Atoi(os.Getenv("DBPort"))
+	res.DBPort = int16(portConv)
+	res.DBName = os.Getenv("DBName")
+	return res
+}
+
 func InitDB(s Setting) *gorm.DB {
 	// format koneksi
 	// username:password@tcp(IP_Database:Port_Database)/Nama_Database
-	conString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", s.DBUser, s.DBPassword)
+	conString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		s.DBUser,
+		s.DBPassword,
+		s.DBHost,
+		s.DBPort,
+		s.DBName)
 	db, err := gorm.Open(mysql.Open(conString), &gorm.Config{})
 
 	if err != nil {
@@ -26,20 +55,4 @@ func InitDB(s Setting) *gorm.DB {
 
 func MigrateDB(conn *gorm.DB) {
 	conn.AutoMigrate(entity.Batch{}, entity.Student{})
-}
-
-type Setting struct {
-	DBUser     string
-	DBPassword string
-}
-
-func ReadFromEnv() Setting {
-	res := Setting{}
-	err := godotenv.Load("local.env")
-	if err != nil {
-		return Setting{}
-	}
-	res.DBPassword = os.Getenv("DBPass")
-	res.DBUser = os.Getenv("DBUser")
-	return res
 }
--- main.go
+++ main.go
@@ -1,77 +1,81 @@
 package main
 
-import "go-db/config"
+import (
+	"fmt"
+	"go-db/config"
+	"go-db/entity"
+)
 
 func main() {
-	config.ReadFromEnv()
-	// conn := config.InitDB
-	// config.MigrateDB(conn())
-	// aksesStudent := entity.AksesStudent{DB: conn()}
-	// aksesKelas := entity.AksesKelas{DB: conn()}
-	// var input int = 0
-	// for input != 99 {
-	// 	fmt.Println("\tSistem pendataan siswa")
-	// 	fmt.Println("1. Tambah Data Siswa")
-	// 	fmt.Println("2. Lihat Data Siswa")
-	// 	fmt.Println("3. Update Data Siswa")
-	// 	fmt.Println("4. Hapus Data Siswa")
-	// 	fmt.Println("5. Lihat Siswa Tertentu")
-	// 	fmt.Println("6. Tambah Batch")
-	// 	fmt.Println("7. Lihat Data Batch")
-	// 	fmt.Println("99. Keluar")
-	// 	fmt.Print("Masukkan Pilihan menu: ")
-	// 	fmt.Scanln(&input)
+	param := config.ReadFromEnv()
+	conn := config.InitDB(param)
+	config.MigrateDB(conn)
+	aksesStudent := entity.AksesStudent{DB: conn}
+	aksesKelas := entity.AksesKelas{DB: conn}
+	var input int = 0
+	for input != 99 {
+		fmt.Println("\tSistem pendataan siswa")
+		fmt.Println("1. Tambah Data Siswa")
+		fmt.Println("2. Lihat Data Siswa")
+		fmt.Println("3. Update Data Siswa")
+		fmt.Println("4. Hapus Data Siswa")
+		fmt.Println("5. Lihat Siswa Tertentu")
+		fmt.Println("6. Tambah Batch")
+		fmt.Println("7. Lihat Data Batch")
+		fmt.Println("99. Keluar")
+		fmt.Print("Masukkan Pilihan menu: ")
+		fmt.Scanln(&input)
 
-	// 	switch input {
-	// 	case 1:
-	// 		var newStudent entity.Student
-	// 		fmt.Print("Masukkan nama: ")
-	// 		fmt.Scanln(&newStudent.Nama)
-	// 		fmt.Print("Masukkan alamat: ")
-	// 		fmt.Scanln(&newStudent.Alamat)
-	// 		fmt.Print("Masukkan nomorhp: ")
-	// 		fmt.Scanln(&newStudent.NomorHP)
-	// 		fmt.Print("Masukkan email: ")
-	// 		fmt.Scanln(&newStudent.Email)
-	// 		fmt.Print("Masukkan ID_Kelas: ")
-	// 		fmt.Scanln(&newStudent.ID_Kelas)
-	// 		res := aksesStudent.TambahMuridBaru(newStudent)
-	// 		if res.ID == 0 {
-	// 			fmt.Println("Tidak bisa input siswa, ada error")
-	// 			break
-	// 		}
-	// 		fmt.Println("Berhasl input siswa")
-	// 	case 2:
-	// 		fmt.Println("Daftar Seluruh Murid")
-	// 		for _, val := range aksesStudent.GetAllData() {
-	// 			fmt.Println(val)
-	// 		}
-	// 	case 4:
-	// 		var ID int
-	// 		fmt.Print("Masukkan ID yang akan dihapus ")
-	// 		fmt.Scanln(&ID)
-	// 		fmt.Println(aksesStudent.HapusMurid(ID))
-	// 	case 5:
-	// 		var ID int
-	// 		fmt.Print("Masukkan ID yang akan dicari ")
-	// 		fmt.Scanln(&ID)
-	// 		fmt.Println(aksesStudent.GetSpecificUser(ID))
-	// 	case 6:
-	// 		var newStudent entity.Batch
-	// 		fmt.Print("Masukkan nama: ")
-	// 		fmt.Scanln(&newStudent.NamaKelas)
+		switch input {
+		case 1:
+			var newStudent entity.Student
+			fmt.Print("Masukkan nama: ")
+			fmt.Scanln(&newStudent.Nama)
+			fmt.Print("Masukkan alamat: ")
+			fmt.Scanln(&newStudent.Alamat)
+			fmt.Print("Masukkan nomorhp: ")
+			fmt.Scanln(&newStudent.NomorHP)
+			fmt.Print("Masukkan email: ")
+			fmt.Scanln(&newStudent.Email)
+			fmt.Print("Masukkan ID_Kelas: ")
+			fmt.Scanln(&newStudent.ID_Kelas)
+			res := aksesStudent.TambahMuridBaru(newStudent)
+			if res.ID == 0 {
+				fmt.Println("Tidak bisa input siswa, ada error")
+				break
+			}
+			fmt.Println("Berhasl input siswa")
+		case 2:
+			fmt.Println("Daftar Seluruh Murid")
+			for _, val := range aksesStudent.GetAllData() {
+				fmt.Println(val)
+			}
+		case 4:
+			var ID int
+			fmt.Print("Masukkan ID yang akan dihapus ")
+			fmt.Scanln(&ID)
+			fmt.Println(aksesStudent.HapusMurid(ID))
+		case 5:
+			var ID int
+			fmt.Print("Masukkan ID yang akan dicari ")
+			fmt.Scanln(&ID)
+			fmt.Println(aksesStudent.GetSpecificUser(ID))
+		case 6:
+			var newStudent entity.Batch
+			fmt.Print("Masukkan nama: ")
+			fmt.Scanln(&newStudent.NamaKelas)
 
-	// 		aksesKelas.TambahKelasBaru(newStudent)
+			aksesKelas.TambahKelasBaru(newStudent)
 
-	// 		fmt.Println("Berhasl input kelas")
-	// 	case 7:
-	// 		fmt.Println("Daftar Seluruh Kelas")
-	// 		fmt.Println(aksesKelas.GetAllKelas())
-	// 	default:
-	// 		continue
-	// 	}
-	// }
-	// fmt.Println("Terima kasih sudah mencoba program saya")
+			fmt.Println("Berhasl input kelas")
+		case 7:
+			fmt.Println("Daftar Seluruh Kelas")
+			fmt.Println(aksesKelas.GetAllKelas())
+		default:
+			continue
+		}
+	}
+	fmt.Println("Terima kasih sudah mencoba program saya")
 
 	// fmt.Println("Nilai dari ping", conn.Ping())
 	// var studentData entity.Student
